080890f13ff8852291bafc8bb382ce2947d69117
Aryan Felix <aryanfelix60@gmail.com>
2020-06-03 02:41:50 +0530

    Initial commit

---
+++ Movie Recommendation.py
@@ -0,0 +1,78 @@
+import json
+import requests
+def get_movies_from_tastedive(s):
+    baseurl = "https://tastedive.com/api/similar"
+    paramlist = {}
+    paramlist["q"] = s
+    paramlist["type"] = "movies"
+    paramlist["limit"] = 5
+    #paramlist["k"] = "Your Key Here" - Optional
+    result = requests.get(baseurl, params = paramlist)
+    result = result.json()
+    return result
+    
+def extract_movie_titles(res):
+    temp = []
+    for x in res["Similar"]["Results"]:
+        temp.append(x["Name"])
+    return temp
+
+def get_related_titles(l):
+    related = []
+    for x in l:
+        temp = get_movies_from_tastedive(x)
+        temp = extract_movie_titles(temp)
+        related.append(temp)
+    final = []
+    for x in related:
+        for y in x:
+            if y not in final:
+                final.append(y)
+    return final
+
+def get_movie_data(s):
+    baseurl = "http://www.omdbapi.com/"
+    paramlist = {}
+    paramlist["t"] = s
+    paramlist["r"] = "json"
+    #paramlist["apikey"] = "Your Key Here" - Optional
+    result = requests.get(baseurl, params = paramlist)
+    result = result.json()
+    return result
+
+def get_movie_rating(res):
+    rat = 0
+    for x in res["Ratings"]:
+        if x["Source"] == "Rotten Tomatoes":
+            rat = x['Value']
+            rat = rat[:-1]
+            rat = int(rat)
+    return rat
+
+def get_sorted_recommendations(l):
+    tits = []
+    rats = []
+    rel = get_related_titles(l)
+    for x in rel:
+        tits.append(x)
+        rats.append(get_movie_rating(get_movie_data(x)))
+    final = list(zip(tits,rats))
+    final.sort(reverse = True, key = lambda a: (a[1],a[0]))
+    finalnew = []
+    for x in final:
+        finalnew.append(x[0])
+    return finalnew
+
+print("WELCOME TO MOVIE RECOMMENDATION SYSTEM")
+print("Feeling bored? Well we are here to help!")
+recomlist = []
+ch = ""
+print("Keep entering movies. Enter -1 to stop.")
+print("Enter movie titles the same as the actual movies.")
+while ch != "-1":
+    ch = input("Enter movie title : ")
+    recomlist.append(ch)
+recoms = get_sorted_recommendations(recomlist)
+print("Your recommended movies are :")
+for i in range(len(recoms)):
+    print("{}. {}".format(i+1, recoms[i]))
---
+++ README.md
@@ -0,0 +1,28 @@
+# Movie Recommendation Software
+ ### Predicts best possible movie choices based on your previous movie likings.
+ 
+ ### Works on the TasteDive and OMDB API.
+
+## Steps to run
+1. ### Run Movie-Recommendation.py
+    
+2. ### Enter a movie name or multiple movie names that you liked.
+    ### **NOTE** - Make sure to enter the exact name as the movie titles.
+
+3. ### API Keys can be found for free in the links below.
+
+4. ### Feedback and Forking is encouraged.
+
+## Links to API documentations
+1. ### TasteDive
+
+    ### https://tastedive.com/read/api
+
+2. ### OMDB
+
+    ### http://www.omdbapi.com/
+
+
+## License
+### **MIT License**
+#### **Created by Aryan Felix**
